185014b0cd0a790a109e950b542514304bb88949
南风有时起 <6583978+the_south_wind_sometimes_blows@user.noreply.gitee.com>
2020-06-29 22:04:13 +0800

    5. 单元测试加用户用户评论

--- app/extensions.py
+++ app/extensions.py
@@ -7,11 +7,20 @@
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import MetaData
 
 
 # Flask-Cors plugin
 cors = CORS()
 # Flask-SQLAlchemy plugin
-db = SQLAlchemy()
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+# Flask-SQLAlchemy plugin
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 # # Flask-Migrate plugin
-migrate = Migrate()
+migrate = Migrate(render_as_batch=True)
